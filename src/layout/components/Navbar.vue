b41fef4b517190bec4e7bc460685fcf0c84ced73
Johnnyyu <johnyu97@hotmail.com>
2021-07-07 14:23:25 +0800

    update

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,9 +7,9 @@
     />
 
     <breadcrumb class="breadcrumb-container" />
-
-    <div class="right-menu">
-      <!-- 登陆用户管理 -->
+    <!-- 登陆用户管理 -->
+    <!-- <div class="right-menu">
+     
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img :src="avatar + '?imageView2/1/w/80/h/80'" class="user-avatar" />
@@ -36,7 +36,7 @@
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
-    </div>
+    </div> -->
   </div>
 </template>
 
